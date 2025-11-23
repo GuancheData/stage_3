aa62b80a8aea71f5c01cc832980ee9f50a1176a6
ayozeruanoalc <ayozeruanoalcantara@gmail.com>
2025-11-23 11:56:17 +0000

    feat: add Docker support for ingestion module

---
+++ ingestion-service/src/main/java/com/guanchedata/Main.java
@@ -0,0 +1,47 @@
+package com.guanchedata;
+
+import com.guanchedata.application.usecases.ingestionservice.BookProviderController;
+import com.guanchedata.infrastructure.adapters.apiservices.BookStatusService;
+import com.guanchedata.infrastructure.adapters.apiservices.IngestBookService;
+import com.guanchedata.infrastructure.adapters.apiservices.ListBooksService;
+import com.guanchedata.infrastructure.adapters.bookprovider.BookDownloadLog;
+import com.guanchedata.infrastructure.adapters.bookprovider.BookStorageDate;
+import com.guanchedata.infrastructure.adapters.bookprovider.GutenbergBookContentSeparator;
+import com.guanchedata.infrastructure.ports.*;
+import com.guanchedata.util.DateTimePathGenerator;
+import io.javalin.Javalin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(args[0]);
+
+        PathGenerator pathGenerator = new DateTimePathGenerator(args[0]);
+        GutenbergBookContentSeparator separator = new GutenbergBookContentSeparator();
+        BookStorage storageDate = new BookStorageDate(pathGenerator, separator);
+        BookDownloadStatusStore bookDownloadLog = new BookDownloadLog(args[1]);
+
+        BookDownloader ingestBookService = new IngestBookService(storageDate, bookDownloadLog);
+        BookListProvider listBooksService = new ListBooksService(bookDownloadLog);
+        BookStatusProvider bookStatusService = new BookStatusService(bookDownloadLog);
+
+        BookProviderController controller = new BookProviderController(
+                ingestBookService,
+                listBooksService,
+                bookStatusService
+        );
+
+        Javalin app = Javalin.create(config -> {
+            config.http.defaultContentType = "application/json";
+        }).start(7001);
+
+        app.post("/ingest/{book_id}", controller::ingestBook);
+        app.get("/ingest/status/{book_id}", controller::status);
+        app.get("/ingest/list", controller::listAllBooks);
+    }
+}
+
---
+++ ingestion-service/src/main/java/com/guanchedata/application/usecases/ingestionservice/BookProviderController.java
@@ -0,0 +1,41 @@
+package com.guanchedata.application.usecases.ingestionservice;
+
+import com.google.gson.Gson;
+import com.guanchedata.infrastructure.ports.BookDownloader;
+import com.guanchedata.infrastructure.ports.BookListProvider;
+import com.guanchedata.infrastructure.ports.BookStatusProvider;
+import io.javalin.http.Context;
+import java.util.Map;
+
+public class BookProviderController {
+    private final BookDownloader ingestBookService;
+    private final BookListProvider listBooksService;
+    private final BookStatusProvider bookStatusService;
+    private static final Gson gson = new Gson();
+
+    public BookProviderController(
+            BookDownloader ingestBookService,
+            BookListProvider listBooksService,
+            BookStatusProvider bookStatusService) {
+        this.ingestBookService = ingestBookService;
+        this.listBooksService = listBooksService;
+        this.bookStatusService = bookStatusService;
+    }
+
+    public void ingestBook(Context ctx) {
+        int bookId = Integer.parseInt(ctx.pathParam("book_id"));
+        Map<String, Object> result = ingestBookService.ingest(bookId);
+        ctx.result(gson.toJson(result));
+    }
+
+    public void listAllBooks(Context ctx) {
+        Map<String, Object> result = listBooksService.list();
+        ctx.result(gson.toJson(result));
+    }
+
+    public void status(Context ctx) {
+        int bookId = Integer.parseInt(ctx.pathParam("book_id"));
+        Map<String, Object> result = bookStatusService.status(bookId);
+        ctx.result(gson.toJson(result));
+    }
+}
